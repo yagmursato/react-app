10109f95424a35a9f13eebb8afb6ca5b57754e03
yagmursato <yagmur_sato@windowslive.com>
2019-11-28 14:33:21 +0300

    graph for currency rating using canvasjs

---
+++ build/precache-manifest.f0883504e302ebf4474e1cf8ce7bfbb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e089d582f450068968550ec5687bce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a2821f85eb9d191ded",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7bba74c2f67295ff87fe",
+    "url": "/static/js/2.961e0020.chunk.js"
+  },
+  {
+    "revision": "f4a2821f85eb9d191ded",
+    "url": "/static/js/main.1ad51bed.chunk.js"
+  },
+  {
+    "revision": "c19d729c9a1acbbc0b3b",
+    "url": "/static/js/runtime-main.74540a05.js"
+  }
+]);
